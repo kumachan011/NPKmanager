04b2822a4e1aa5033debe0d9a17cd399f5357b24
kumachan011 <kumachanart_011@gmail.com>
2026-08-25 03:37:34 +0300

    update

--- filemanager/manip.cpp
+++ filemanager/manip.cpp
@@ -9,6 +9,8 @@
 #include "plusaes.hpp"
 
 unsigned char byte;
+unsigned long int fileSize;
+const int headerSize = 32;
 std::string filePath;
 
 struct NPKgame {
@@ -28,24 +30,26 @@ NPKgame games[] = {
 
 struct NPKentry {
 public:
-	std::vector<unsigned char> fileNameLength;
+	std::vector<unsigned char> fileNameLength; // first 3 bytes, if byte 1 is 01 that means the file is compressed (mostly game dialogue
 
-	std::vector<unsigned char> fileName;
+	std::string fileName; // represents the length pulled from fileNameLength
 
-	std::vector<unsigned char> realSize;
+	std::vector<unsigned char> realSize; // the files real size after decryption and compression, should be 4 bytes RIGHT after fileName, little endian
 
-	std::vector<unsigned char> SHA256;// not important for now
+	std::vector<unsigned char> SHA256;// not important for now, represents 32 bytes
 
-	std::vector<unsigned char> sectionSize;
+	std::vector<unsigned char> sectionSize; // 4 bytes right after the SHA256, 1 section represents 64 kilobytes, or 0x10000/65536 bytes
 
-	struct segmentData {
-		std::vector<unsigned char> offset;
+	struct segmentData { // the amount of segmentData structs is created depending on sectionSize amount
+		std::vector<unsigned char> offset; // the offset of the ACTUAL data of the entry in the file
 
-		std::vector<unsigned char> alignedSize;
+		std::vector<unsigned char> alignedSize; // how big the file is with PKCS5 padding (fancy word for adding numbers at the
+												//end so the decryption block is 16 bytes)
 
-		std::vector<unsigned char> compressedSize;
+		std::vector<unsigned char> compressedSize; // how big the file actually is without the padding (will be important for decompression if thats the case)
 	};
 
+	std::string fn = this->fileName.erase(0, this->fileName.rfind("/") + 1);
 };
 
 
@@ -53,9 +57,9 @@ class NPKmanip // all the file manipulation functions
 {
 
 
-private: //file header container
+private: // will need to improve this
 	std::vector<unsigned char> NPKheader;
-	std::string absoluteFilePath;
+	std::vector<unsigned char> fileArr;
 
 	unsigned char NPKver[8] = {}; // not needed yet
 	unsigned char iv[16] = {};
@@ -66,14 +70,12 @@ public:
 	NPKmanip(const std::string& filename) {
 		readFile.open(filename, std::ios::binary);
 	}
-	std::string file;
+
 	unsigned int dataOffsetDec = NULL;
 	unsigned int entryNumberDec = NULL;
 
-
-	std::vector <unsigned char> dataBuffer;
-	std::vector<unsigned char> entryBuffer;
-
+	std::vector <unsigned char> entryBuffer;
+	std::vector<unsigned char> entryDataBuffer;
 
 	std::ifstream readFile;
 	std::ofstream writeFile;
@@ -81,11 +83,10 @@ public:
 public:
 	void readHeader()
 	{
-		int headerSize = 32;
-		while (readFile >> std::noskipws >> byte && headerSize > 0) {
+		int temphHeaderSize = headerSize;
+		while (readFile >> std::noskipws >> byte && temphHeaderSize > 0) {
 			NPKheader.push_back(byte);
-			//std::cout << byte << " ";
-			headerSize--;
+			temphHeaderSize--;
 		}
 		for (int i = 8; i < 24; i++) { //will be improved eventually
 			iv[i - 8] = NPKheader[i];
@@ -100,148 +101,132 @@ public:
 		std::memcpy(&dataOffsetDec, dataOffset, 4);
 	}
 
-	void decrypt(std::ifstream &file, int game, unsigned int startOffset, unsigned int endOffset, std::string path, int type) {
-
-		entryBuffer.clear();
+	void decrypt(std::ifstream &file, int game, unsigned int startOffset, unsigned int endOffset, int type, std::string fileName) {
 		
-
-		std::vector<unsigned char> fileArr;
 		readFile.seekg(startOffset);
 		for (unsigned int i = 0; i < endOffset; i++) {
-			//std::cout << (unsigned int)byte;
 			readFile >> byte;
 			fileArr.push_back(byte);
 		}
-		std::cout << "done";
-		dataBuffer.resize(fileArr.size());
-		std::cout << "starting decryption";
-		plusaes::decrypt_cbc(&fileArr[0], (long)fileArr.size(), &games[game].key[0], (long)32, &iv, &dataBuffer[0], (long)dataBuffer.size(), NULL);
 
-		std::cout << "ended decryption";
-
-		std::filesystem::path p(filePath);
-		p = p.parent_path();
-		std::string absoluteFilePath = p.string();
-
-		writeFile.open("C:\\Users\\Kuma\\Desktop\\fun\\fun.npk", std::ios::binary); // will be removed eventually
 		if (type == 0) {
-			for (unsigned int i = 0; i < dataBuffer.size(); i++) {
-				writeFile << dataBuffer[i];
-				//std::cout << (unsigned int)dataBuffer[i] << " ";
-			}
+			entryBuffer.resize(fileArr.size());
+			plusaes::decrypt_cbc(&fileArr[0], (long)fileArr.size(), &games[game].key[0], (long)32, &iv, &entryBuffer[0], (long)fileArr.size(), NULL);
+
+			//writeFile.open("C:\\Users\\Kuma\\Desktop\\fun\\fun.npk", std::ios::binary);
+			//for (unsigned int i = 0; i < fileArr.size(); i++) {
+				//writeFile << entryBuffer[i];
+			//}
+			writeFile.close();
+			fileArr.clear();
 		}
-		else {
-			for (unsigned int i = 0; i < dataBuffer.size(); i++) {
-				writeFile << entryBuffer[i];
-				//std::cout << (unsigned int)dataBuffer[i] << " ";
+		else if (type == 1) {
+			entryDataBuffer.resize(fileArr.size());
+			plusaes::decrypt_cbc(&fileArr[0], (long)fileArr.size(), &games[game].key[0], (long)32, &iv, &entryDataBuffer[0], (long)fileArr.size(), 0);
+			writeFile.open("C:\\Users\\Kuma\\Desktop\\fun\\" + fileName, std::ios::binary);
+			for (unsigned int i = 0; i < fileArr.size(); i++) {
+				writeFile << entryDataBuffer[i];
 			}
+			writeFile.close();
+			fileArr.clear();
 		}
-		
-		std::cout << "File finished writing at path >> " << filePath + "~";
-		writeFile.close();
 	}
 
-	//std::vector<unsigned char> &entries
 	void getEntries(std::vector<unsigned char> &entries) {
 		long int nextEntryOffset = 0;
+		int entryIncrementor = 0;
 		int sectionAmount = 0;
 		int sectionFileAmount = 0;
 
+		unsigned long long startOffsetDec = 0;
+		unsigned long endOffsetDec = 0;
+		
+		std::vector<unsigned char> tempEntryData;
+
+
 		for (unsigned int i = 0; i < entryNumberDec; i++) {
-			int entryIncrementor = i;
+			entryIncrementor = i;
 			sectionAmount = 0;
 
-			std::cout << "\nArchived entry: ";
-
-			std::vector<unsigned char> tempEntryData;
 			unsigned int fLen = (unsigned int)entries[nextEntryOffset + 1];
 
-			for (int i = nextEntryOffset; i < fLen + nextEntryOffset + 63; i++) {
+			for (unsigned int i = nextEntryOffset; i < fLen + nextEntryOffset + 63; i++) {
 				tempEntryData.push_back(entries[i]);
-				//std::cout << (unsigned int)tempEntryData[i] << " ";
 			}
+
 			NPKentry entry{
 				std::vector<unsigned char>(tempEntryData.begin(), tempEntryData.begin() + 3),
-				std::vector<unsigned char>(tempEntryData.begin() + 3, tempEntryData.begin() + 3 + fLen),
+				std::string(tempEntryData.begin() + 3, tempEntryData.begin() + 3 + fLen),
 				std::vector<unsigned char>(tempEntryData.begin() + 3 + fLen, tempEntryData.begin() + 7 + fLen),
 				std::vector<unsigned char>(tempEntryData.begin() + 7 + fLen, tempEntryData.begin() + 39 + fLen),
 				std::vector<unsigned char>(tempEntryData.begin() + 39 + fLen, tempEntryData.begin() + 41 + fLen)
 			};
 
-			for (int i = 0; i < entry.fileName.size(); i++) {
-				std::cout << entry.fileName[i];
-			}
+			//std::string fn = "f.png";//std::filesystem::path(entry.fileName).filename().string();
 
 			if ((unsigned int)entry.sectionSize[0] > 1) {
 				std::cout << "\nSection size over 1, enabling file sectioning...";
-				for (int i = 0; i < (unsigned int)entry.sectionSize[0]; i++) {
+				for (unsigned int i = 0; i < (unsigned int)entry.sectionSize[0]; i++) {
 					NPKentry::segmentData segment[]{
 						std::vector<unsigned char>(tempEntryData.begin() + 43 + fLen, tempEntryData.begin() + 51 + fLen),
 						std::vector<unsigned char>(tempEntryData.begin() + 51 + fLen, tempEntryData.begin() + 55 + fLen),
 						std::vector<unsigned char>(tempEntryData.begin() + 55 + fLen, tempEntryData.begin() + 57 + fLen)
 					};
-					/*unsigned long startOffsetDec;
+					unsigned long startOffsetDec;
 					unsigned long endOffsetDec;
 					std::memcpy(&startOffsetDec, &segment->offset, 8);
 					std::memcpy(&endOffsetDec, &segment->alignedSize, 4);
-					std::cout << startOffsetDec << " " << endOffsetDec;
-					decrypt(readFile, 0, startOffsetDec, endOffsetDec, std::string(entry.fileName.begin(), entry.fileName.end()));*/
+					//std::cout << startOffsetDec << " " << endOffsetDec;
+					//decrypt(readFile, 0, startOffsetDec + headerSize, endOffsetDec + headerSize, std::string(entry.fileName.begin(), entry.fileName.end()));
 					
 					//decrypt(NPK.readFile, )
 					sectionAmount++;
 					sectionFileAmount++;
 				}
 			} else if ((unsigned int)entry.sectionSize[0] == 1) {
-				NPKentry::segmentData segment{
+				NPKentry::segmentData segment{	
 					std::vector<unsigned char>(tempEntryData.begin() + 43 + fLen, tempEntryData.begin() + 51 + fLen),
 					std::vector<unsigned char>(tempEntryData.begin() + 51 + fLen, tempEntryData.begin() + 55 + fLen),
 					std::vector<unsigned char>(tempEntryData.begin() + 55 + fLen, tempEntryData.begin() + 57 + fLen)
 				};
-				for (int i = 0; i < segment.alignedSize.size(); i++) {
-					std::cout << (unsigned int)segment.alignedSize[i] << " ";
-				}
-				unsigned long startOffsetDec;
-				unsigned long endOffsetDec;
-				std::memcpy(&startOffsetDec, segment.offset.data(), 8);
-				std::memcpy(&endOffsetDec, segment.alignedSize.data(), 4);
-				std::cout << startOffsetDec << " " << endOffsetDec + startOffsetDec;
-				decrypt(readFile, 0, startOffsetDec, endOffsetDec + startOffsetDec, std::string(entry.fileName.begin(), entry.fileName.end()), 1);
+				std::memcpy(&startOffsetDec, segment.offset.data(), segment.offset.size());
+				std::memcpy(&endOffsetDec, segment.alignedSize.data(), segment.alignedSize.size());
+				decrypt(readFile, 0, startOffsetDec, endOffsetDec, 1, entry.fn);
 				sectionAmount = 1;
 			}
 
 			nextEntryOffset = nextEntryOffset + fLen + 43 + sectionAmount * 20;
-			
-			 // the program will fetch the nextEntryOffset by calculating everything in the NPKentry + the last entry offset, and itll make
-			// an array of all entry data, then it will call the void decrypt() function in order to decrypt all those entries. furthermore, if needed
-			// they will be decompressed in void decompress()
+			std::cout << "Written entry " << entryIncrementor << " | " << entryNumberDec << ": " << entry.fn <<"\n";
+			tempEntryData.clear();
 		}
-
 		std::cout << "\nDecrypted file had " << entryNumberDec << " entries, with a total of " << sectionFileAmount << " files over 64kb.\n";
 	}
 
 	void decompress() {
-
+		//blahblahblah
 	}
 };
 
 int main()
 {
 
-	while (!std::filesystem::exists(filePath)) {
-		std::cout << "\nPlease insert the path of your NPK file: ";
+	while (std::filesystem::path(filePath).extension().string() != ".npk") {
+		std::cout << "\nPlease write/drop the path of your NPK file: ";
 		std::getline(std::cin, filePath);
+
 		if (!std::filesystem::exists(filePath)) {
-			std::cout << "\nYour file path is not valid/doesn't exist! \n";
+			std::cout << "\nYour file path is not valid/doesn't exist!";
+		} else if (std::filesystem::path(filePath).extension().string() != ".npk") {
+			std::cout << "\nWritten/dropped file is not an NPK!";
 		}
 	}
 	
 	NPKmanip NPK(filePath);
-	NPK.file = filePath;
-
 
 	NPK.readHeader();
-	NPK.decrypt(NPK.readFile, 0, 32, NPK.dataOffsetDec, "C:\\Users\\Kuma\\Desktop\\totono\\You and Me and Her", 0);
-	NPK.getEntries(NPK.dataBuffer);
+	std::string null = "Null";
+	NPK.decrypt(NPK.readFile, 0, headerSize, NPK.dataOffsetDec, 0, null);
+	NPK.getEntries(NPK.entryBuffer);
 
 
 
